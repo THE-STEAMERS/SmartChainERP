fee3a160ddce5435fa2915a0954aac7c354be374
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-03-25 04:16:38 +0530

    anomaly message in frontend

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smartchain_db',
-        'USER': 'smartchain',
-        'PASSWORD': 'venkat*2005',
+        'NAME': 'postgres',
+        'USER': 'joshuaa',
+        'PASSWORD': 'Sanjos123*',
         'HOST': 'localhost',  # If running PostgreSQL locally
         'PORT': '5432',       # Default PostgreSQL port
     }
